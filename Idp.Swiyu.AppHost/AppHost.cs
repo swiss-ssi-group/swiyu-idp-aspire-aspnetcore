42b5665db81d4ae963d8dff9a4e56ac8e4b4a749
damienbod <damien_bod@hotmail.com>
2025-10-10 14:15:38 +0200

    Remove API , not required

--- Idp.Swiyu.AppHost/AppHost.cs
+++ Idp.Swiyu.AppHost/AppHost.cs
@@ -2,7 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var IDENTITY_PROVIDER = "identityProvider";
 var WEB_CLIENT = "webClient";
-var API_SERVICE = "apiService";
 var CACHE = "cache";
 
 var cache = builder.AddRedis(CACHE);
@@ -12,16 +11,11 @@ var identityProvider = builder.AddProject<Projects.Idp_Swiyu_IdentityProvider>(I
     .WithReference(cache)
     .WaitFor(cache);
 
-var apiService = builder.AddProject<Projects.Idp_Swiyu_ApiService>(API_SERVICE)
-    .WithHttpHealthCheck("/health");
-
 builder.AddProject<Projects.Idp_Swiyu_Web>(WEB_CLIENT)
     .WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health")
     .WithReference(cache)
     .WaitFor(cache)
-    .WithReference(apiService)
-    .WaitFor(apiService)
     .WaitFor(identityProvider);
 
 builder.Build().Run();
