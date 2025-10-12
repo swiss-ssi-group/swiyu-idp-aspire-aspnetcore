1bb2725f62488e941d4cbe75022eecdf0b6098ef
damienbod <damien_bod@hotmail.com>
2025-10-12 22:49:56 +0200

    Init Swiyu login

---
+++ Idp.Swiyu.IdentityProvider/Pages/Swiyu/Login.cshtml.cs
@@ -0,0 +1,88 @@
+using Duende.IdentityServer.Services;
+using Duende.IdentityServer.Stores;
+using Idp.Swiyu.IdentityProvider.Models;
+using Idp.Swiyu.IdentityProvider.SwiyuServices;
+using ImageMagick;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualBasic;
+using Net.Codecrete.QrCodeGenerator;
+using System.Text.Json;
+
+namespace Idp.Swiyu.IdentityProvider.Pages.Login;
+
+[SecurityHeaders]
+[AllowAnonymous]
+public class LoginModel : PageModel
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IIdentityServerInteractionService _interaction;
+    private readonly IEventService _events;
+    private readonly IAuthenticationSchemeProvider _schemeProvider;
+    private readonly IIdentityProviderStore _identityProviderStore;
+
+    [BindProperty]
+    public InputModel Input { get; set; } = default!;
+
+    private readonly VerificationService _verificationService;
+    private readonly string? _swiyuOid4vpUrl;
+
+    [BindProperty]
+    public string? VerificationId { get; set; }
+
+    [BindProperty]
+    public string? QrCodeUrl { get; set; } = string.Empty;
+
+    [BindProperty]
+    public byte[] QrCodePng { get; set; } = [];
+
+    public LoginModel(
+        IIdentityServerInteractionService interaction,
+        IAuthenticationSchemeProvider schemeProvider,
+        IIdentityProviderStore identityProviderStore,
+        IEventService events,
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager,
+        VerificationService verificationService,
+        IConfiguration configuration)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _interaction = interaction;
+        _schemeProvider = schemeProvider;
+        _identityProviderStore = identityProviderStore;
+        _events = events;
+
+        _verificationService = verificationService;
+        _swiyuOid4vpUrl = configuration["SwiyuOid4vpUrl"];
+        QrCodeUrl = QrCodeUrl.Replace("{OID4VP_URL}", _swiyuOid4vpUrl);
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        // check if we are in the context of an authorization request
+        var context = await _interaction.GetAuthorizationContextAsync(Input.ReturnUrl);
+
+        var presentation = await _verificationService
+            .CreateBetaIdVerificationPresentationAsync();
+
+        var verificationResponse = JsonSerializer.Deserialize<CreateVerificationPresentationModel>(presentation);
+        // verification_url
+        QrCodeUrl = verificationResponse!.verification_url;
+
+        var qrCode = QrCode.EncodeText(verificationResponse!.verification_url, QrCode.Ecc.Quartile);
+        QrCodePng = qrCode.ToPng(20, 4, MagickColors.Black, MagickColors.White);
+
+        VerificationId = verificationResponse.id;
+
+        return Page();
+    }
+}
