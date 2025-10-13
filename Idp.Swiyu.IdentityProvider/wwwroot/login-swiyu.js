7acb72209f65bbcfc21f047f804261087fc6c359
damienbod <damien_bod@hotmail.com>
2025-10-13 22:06:38 +0200

    clean up

--- Idp.Swiyu.IdentityProvider/wwwroot/login-swiyu.js
+++ Idp.Swiyu.IdentityProvider/wwwroot/login-swiyu.js
@@ -3,19 +3,14 @@ const verificationId = document.getElementById('verificationId');
 const form = document.getElementById("pollSwiyu");
 const buttonPollSwiyu = document.getElementById("buttonPollSwiyu");
 
-
 if (verificationId != null) {
 
     let checkStatus = setInterval(function () {
         if (verificationId) {
-
-            alert("hi")
             if (form) {
                 form.submit();
             }
             clearInterval(checkStatus)
-
         }
-
     }, 1500);
 }
