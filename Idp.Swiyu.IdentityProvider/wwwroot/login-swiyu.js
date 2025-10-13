467d7988bfb3f1fa489c99f052bab9576aeffc70
damienbod <damien_bod@hotmail.com>
2025-10-13 19:29:33 +0200

    Test

---
+++ Idp.Swiyu.IdentityProvider/wwwroot/login-swiyu.js
@@ -0,0 +1,21 @@
+﻿
+$(document).ready(function () {
+    //dom is ready
+
+    const form = document.getElementById("pollSwiyu");
+    const buttonPollSwiyu = document.getElementById("buttonPollSwiyu");
+
+    function pollSwiyuSubmit(e) {
+        e.preventDefault();
+
+        if (form) {
+            form.submit();
+        }
+ 
+        return false;
+    }
+
+    if (buttonPollSwiyu) {
+        buttonPollSwiyu.onclick = pollSwiyuSubmit;
+    }
+});
