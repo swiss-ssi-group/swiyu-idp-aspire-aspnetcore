753dae96c481ab297ff62693d7a52a7622e3da2b
damienbod <damien_bod@hotmail.com>
2025-10-13 22:31:06 +0200

    Update blazor

--- Idp.Swiyu.Web/Program.cs
+++ Idp.Swiyu.Web/Program.cs
@@ -1,81 +1,101 @@
-using Idp.Swiyu.Web;
 using Idp.Swiyu.Web.Components;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Idp.Swiyu.Web;
 
-// Add service defaults & Aspire client integrations.
-builder.AddServiceDefaults();
-builder.AddRedisOutputCache("cache");
-
-builder.Services.AddAuthentication(options =>
+public class Program
 {
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-})
-.AddCookie()
-.AddOpenIdConnect(options =>
-{
-    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.Scope.Add("profile");
-    options.Scope.Add("apiscope");
-    options.Scope.Add("offline_access");
-
-    var oidcConfig = builder.Configuration.GetSection("OpenIDConnectSettings");
-    options.Authority = oidcConfig["Authority"];
-    options.ClientId = oidcConfig["ClientId"];
-    options.ClientSecret = oidcConfig["ClientSecret"];
-    options.ResponseType = OpenIdConnectResponseType.Code;
-    options.MapInboundClaims = false;
-    options.TokenValidationParameters.NameClaimType = "name";
-    options.TokenValidationParameters.RoleClaimType = "role";
-
-    options.SaveTokens = true;
-});
-
-var requireAuthPolicy = new AuthorizationPolicyBuilder()
-    .RequireAuthenticatedUser()
-    .Build();
-
-builder.Services.AddAuthorizationBuilder()
-    .SetFallbackPolicy(requireAuthPolicy);
-
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
+    public static void Main(string[] args)
     {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https+http://apiservice");
-    });
+        var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+        // Add service defaults & Aspire client integrations.
+        builder.AddServiceDefaults();
+        builder.AddRedisOutputCache("cache");
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        builder.Services.AddHttpContextAccessor();
+
+        var oidcConfig = builder.Configuration.GetSection("OpenIDConnectSettings");
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            options.DefaultSignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        })
+        .AddCookie(options =>
+        {
+            options.Cookie.Name = "__Host-idp-swiyu-web";
+            options.Cookie.SameSite = SameSiteMode.Lax;
+            // can be strict if same-site
+            //options.Cookie.SameSite = SameSiteMode.Strict;
+        })
+        .AddOpenIdConnect(options =>
+        {
+            builder.Configuration.GetSection("OpenIDConnectSettings").Bind(options);
+
+            options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.ResponseType = OpenIdConnectResponseType.Code;
+
+            options.SaveTokens = true;
+            options.GetClaimsFromUserInfoEndpoint = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                NameClaimType = "name"
+            };
+        });
+
+        builder.Services.AddAntiforgery(options =>
+        {
+            options.HeaderName = "X-XSRF-TOKEN";
+            options.Cookie.Name = "__Host-core-X-XSRF-TOKEN";
+            options.Cookie.SameSite = SameSiteMode.Strict;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        });
+
+        builder.Services.AddSecurityHeaderPolicies()
+            .SetDefaultPolicy(SecurityHeadersDefinitions
+            .GetHeaderPolicyCollection(oidcConfig["Authority"],
+                builder.Environment.IsDevelopment()));
+
+        builder.Services.AddAuthenticationCore();
+        builder.Services.AddAuthorization();
+        builder.Services.AddCascadingAuthenticationState();
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            app.UseHsts();
+        }
+        else
+        {
+            IdentityModelEventSource.ShowPII = true;
+            IdentityModelEventSource.LogCompleteSecurityArtifact = true;
+        }
+
+        app.UseSecurityHeaders();
+
+        app.UseHttpsRedirection();
+        app.UseAntiforgery();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.MapLoginLogoutEndpoints();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAntiforgery();
-
-app.UseOutputCache();
-
-app.MapStaticAssets();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.MapDefaultEndpoints();
-
-app.Run();
